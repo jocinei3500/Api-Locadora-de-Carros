cf5e83167763740e1c0e42b0638fb933821ac4fc
Jocinei da Rosa <jocinei300@gmail.com>
2022-08-24 19:51:38 -0300

    Iniciando api Locadora de carros

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Marca;
+use Illuminate\Http\Client\ResponseSequence;
+use Illuminate\Http\Request;
+use Symfony\Component\Console\Input\Input;
+
+class MarcaController extends Controller
+{
+
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $marcas = $this->marca->all();
+        return response()->json($marcas, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+
+        $imagem=$request->file('imagem');
+        $imagem_urn=$imagem->store('imagem','public');
+        $marca= $this->marca->create([
+            'nome'=>$request->nome,
+            'imagem'=>$imagem_urn
+        ]);
+
+        return response()->json($marca, 200);
+        
+        //$marca = $this->marca->create($request->all());
+        //return response()->json($marca, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Integer
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response()->json(['Erro' => 'Marca não encontrada'], 404);
+        }
+        return $marca;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  Integer $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $marca = $this->marca->find($id);
+
+        if ($marca === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+
+            $regrasDinamicas = array();
+
+            //percorrendo todas as regras definidas no Model
+            foreach ($marca->rules() as $input => $regra) {
+
+                //coletar apenas as regras aplicáveis aos parâmetros parciais da requisição PATCH
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+
+            $request->validate($regrasDinamicas, $marca->feedback());
+        } else {
+            $request->validate($marca->rules(), $marca->feedback());
+        }
+
+
+        $marca->update($request->all());
+        return response()->json($marca, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  Integer $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response()->json(['Erro' => 'Registro não encontrado'], 404);
+        }
+        $marca->delete();
+    }
+}
---
+++ app/Models/Marca.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Marca extends Model
+{
+    use HasFactory;
+    protected $fillable=['nome', 'imagem'];
+
+    public function rules(){
+        return [
+            'nome'=>'required|unique:marcas,nome,'.$this->id,
+            'imagem'=>'required|mimes:png',
+            
+        ];
+ 
+    }
+
+    public function feedback(){
+        return [
+            'required'=>'O campo :attribute não pode estar vazio!',
+            'unique'=>'Já existe um :attribute com esse nome!',
+            'min'=>'O :attribute deve ter pelo menos 10 letras',
+            'mimes'=>'A Imagem precisa ser no formato png'
+        ];
+
+    }
+}
